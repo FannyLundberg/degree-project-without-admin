d74a4ebfc0d744b41f7a2276fafd9f08dc45c873
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2023-02-05 10:52:12 +0100

    Updated tests after changed URL:s and removed admin tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,25 +1,5 @@
 import {} from "cypress";
 
-describe("Login as admin", () => {
-  it("should be possible to login right credentials", () => {
-    cy.visit("/admin");
-    cy.location("pathname").should("include", "admin");
-    cy.dataCy("usernameAdmin").type("Fanny");
-    cy.dataCy("usernamePassword").type("Fanny1224");
-    cy.dataCy("loginSubmit").click();
-    cy.location("pathname").should("include", "loggedIn");
-  });
-
-  it("should not be possible to login with wrong credentials", () => {
-    cy.visit("/admin");
-    cy.location("pathname").should("include", "admin");
-    cy.dataCy("usernameAdmin").type("Fanny");
-    cy.dataCy("usernamePassword").type("FelLösenord");
-    cy.dataCy("loginSubmit").click();
-    cy.location("pathname").should("not.include", "loggedIn");
-  });
-});
-
 describe("Specific elements", () => {
   it("should have a header, a footer and only one H1", () => {
     cy.visit("/");
@@ -27,22 +7,22 @@ describe("Specific elements", () => {
     cy.get("h1").should("have.length", 1);
     cy.get("footer").should("be.visible");
 
-    cy.visit("/apply");
+    cy.visit("/ansoka");
     cy.get("header").should("be.visible");
     cy.get("h1").should("have.length", 1);
     cy.get("footer").should("be.visible");
 
-    cy.visit("/contribute");
+    cy.visit("/bidra");
     cy.get("header").should("be.visible");
     cy.get("h1").should("have.length", 1);
     cy.get("footer").should("be.visible");
 
-    cy.visit("/sponsors");
+    cy.visit("/sponsorer");
     cy.get("header").should("be.visible");
     cy.get("h1").should("have.length", 1);
     cy.get("footer").should("be.visible");
 
-    cy.visit("/about");
+    cy.visit("/om");
     cy.get("header").should("be.visible");
     cy.get("h1").should("have.length", 1);
     cy.get("footer").should("be.visible");
@@ -53,6 +33,6 @@ describe("Functional navigation", () => {
   it("should be possible to navigate with menu", () => {
     cy.visit("/");
     cy.get("header").find("nav").find("a").first().click();
-    cy.location("pathname").should("include", "apply");
+    cy.location("pathname").should("include", "ansoka");
   });
 });
